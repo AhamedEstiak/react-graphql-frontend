7225cc0a0dfb3816eb2f25d7c4203dd5f19db684
estiak <aiubpolin@gmail.com>
2019-02-21 14:59:05 +0600

    update item

--- components/CreateItem.js
+++ components/CreateItem.js
@@ -30,7 +30,7 @@ class CreateItem extends Component {
         title: 'Best shoes',
         price: '100',
         description: 'Love this shoes',
-        image: 'dog.jpg',
+        image: '',
         largeImage: 'large-dog.jpg'
     }
 
@@ -39,6 +39,24 @@ class CreateItem extends Component {
         const val = type === "number" ? parseFloat(value) : value;
         this.setState({
             [name]: val
+        })
+    };
+
+    uploadFile = async e => {
+        const files = e.target.files;
+        const data = new FormData();
+        data.append('file', files[0]);
+        data.append('upload_preset', 'sickfits');
+        
+        const res = await fetch('https://api.cloudinary.com/v1_1/estiak007/image/upload', {
+            method: 'POST',
+            body: data
+        });
+        const file = await res.json();
+        console.log(file);
+        this.setState({
+            image: file.secure_url,
+            largeImage: file.eager[0].secure_url
         })
     };
 
@@ -61,6 +79,18 @@ class CreateItem extends Component {
       }>
       <Error error={error} /> 
         <fieldset disabled={loading} aria-busy={loading}>
+            <label htmlFor="file">
+            Image
+            <input 
+            type="file"
+            id="file"
+            name="file"
+            placeholder="Upload an Image"
+            required
+            onChange={this.uploadFile}
+            />
+            {this.state.image && <img src={this.state.image} alt="Upload preview" width="200" />}
+            </label>
             <label htmlFor="title">
             Title
             <input 
@@ -69,6 +99,7 @@ class CreateItem extends Component {
             name="title"
             placeholder="Title"
             value={this.state.title}
+            required
             onChange={this.handleChange}
             />
             </label>
@@ -80,6 +111,7 @@ class CreateItem extends Component {
             name="price"
             placeholder="Price"
             value={this.state.price}
+            required
             onChange={this.handleChange}
             />
             </label>
@@ -90,6 +122,7 @@ class CreateItem extends Component {
             name="description"
             placeholder="Description"
             value={this.state.description}
+            required
             onChange={this.handleChange}
             />
             </label>
@@ -104,3 +137,4 @@ class CreateItem extends Component {
 }
 
 export default  CreateItem;
+export { CREATE_ITEM_MUTATION };
